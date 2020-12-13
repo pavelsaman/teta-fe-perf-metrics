257ba417fbf58046b7a0dfd00dfe9042aece88cc
Pavel Saman <pavelsam@centrum.cz>
2020-12-13 16:48:24 +0100

    Create selectors via Object.defineProperties()

--- selectors.js
+++ selectors.js
@@ -1,16 +1,67 @@
 
-module.exports = {
-    email            : '#Input_Username',
-    password         : '#Input_Password',
-    logIn            : '.btn.btn-primary',
-    packing          : '#menu-packing-link',
-    logOut           : '[href="/Account/Logout"]',
-    freeLocation     : '.btn.btn-light.btn-block',
-    takenLocation    : '.btn.btn-light.btn-block.confirm',
-    pickedOrder      : '.btn.btn-light.btn-block',
-    confirmOrder     : '[href="/Packing/Parcels"]',
-    confirmParcels   : '#confirm-parcels',
-    parcelWeightInput: '.form-control.parcel-weight',
-    printLabel       : '.btn.btn-primary.btn-print-label',
-    finishPacking    : '#btn-end-packing'
+const immutable = {
+    enumerable  : true,
+    writtable   : false,
+    configurable: false
 };
+
+const selectors = Object.defineProperties(
+    {},
+    {
+        email: {
+            value: '#Input_Username',
+            ...immutable
+        },
+        password: {
+            value: '#Input_Password',
+            ...immutable
+        },
+        logIn: {
+            value: '.btn.btn-primary',
+            ...immutable
+        },
+        packing: {
+            value: '#menu-packing-link',
+            ...immutable
+        },
+        logOut: {
+            value: '[href="/Account/Logout"]',
+            ...immutable
+        },
+        freeLocation: {
+            value: '.btn.btn-light.btn-block',
+            ...immutable
+        },
+        takenLocation: {
+            value: '.btn.btn-light.btn-block.confirm',
+            ...immutable
+        },
+        pickedOrder: {
+            value: '.btn.btn-light.btn-block',
+            ...immutable
+        },
+        confirmOrder: {
+            value: '[href="/Packing/Parcels"]',
+            ...immutable
+        },
+        confirmParcels: {
+            value: '#confirm-parcels',
+            ...immutable
+        },
+        parcelWeightInput: {
+            value: '.form-control.parcel-weight',
+            ...immutable
+        },
+        printLabel: {
+            value: '.btn.btn-primary.btn-print-label',
+            ...immutable
+        },
+        finishPacking: {
+            value: '#btn-end-packing',
+            ...immutable
+        }
+    }
+);
+
+
+module.exports = selectors;
